62050ddfe28b7f97524162e6bea616aa22960091
ayame113 <40050810+ayame113@users.noreply.github.com>
2022-08-10 19:42:57 +0900

    update index page

--- serve.ts
+++ serve.ts
@@ -1,4 +1,10 @@
 import { serve } from "https://deno.land/std@0.151.0/http/server.ts";
+import { serveFile } from "https://deno.land/std@0.151.0/http/file_server.ts";
 import { serveDirWithTs } from "https://deno.land/x/ts_serve@v1.3.0/mod.ts";
 
-serve((request) => serveDirWithTs(request));
+serve((request) => {
+  if (new URL(request.url).pathname === "/") {
+    return serveFile(request, "./index.html");
+  }
+  return serveDirWithTs(request);
+});
